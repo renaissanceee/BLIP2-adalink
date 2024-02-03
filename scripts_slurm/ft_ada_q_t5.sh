cdb94785a3318cdf95e1a0af998ec23aa27c6fa9
renaissanceee <st179481@stud.uni-stuttgart.de>
2024-02-03 18:54:17 +0000

    config

--- scripts_slurm/ft_ada_q_t5.sh
+++ scripts_slurm/ft_ada_q_t5.sh
@@ -7,7 +7,6 @@
 #SBATCH --gres=gpu:2
 #SBATCH --cpus-per-task=16
 #SBATCH --error=/nfs/data2/zhangya/slurm/test_ada_q_t5.e
-#SBATCH --nodelist=worker-6
 
 source /home/stud/zhangya/jiameng/BLIP2-adalink/env/bin/activate
 python -m torch.distributed.run --nproc_per_node=1 /home/stud/zhangya/jiameng/BLIP2-adalink/train.py --cfg-path /home/stud/zhangya/jiameng/BLIP2-adalink/config_blip2/train/okvqa_ada_q_t5.yaml
