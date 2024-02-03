88e1c4f26a22b64455e3c7b7731d7eb1a3f1f946
renaissanceee <st179481@stud.uni-stuttgart.de>
2024-02-03 12:04:21 +0000

    config

--- scripts_slurm/eval_qformer_opt.sh
+++ scripts_slurm/eval_qformer_opt.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 #SBATCH --job-name fed
-#SBATCH --output=eval_qforemr_opt.out
+#SBATCH --output=/nfs/data2/zhangya/slurm/eval_qforemr_opt.out
 #SBATCH --ntasks=1
 #SBATCH --time=200:00:00
 #SBATCH --gres=gpu:2
 #SBATCH --cpus-per-task=16
-#SBATCH --error=eval_qforemr_opt.e
+#SBATCH --error=/nfs/data2/zhangya/slurm/eval_qforemr_opt.e
 #SBATCH --nodelist=worker-7
 
-source ./env/bin/activate
-python -m torch.distributed.run --nproc_per_node=1 evaluate.py --cfg-path config_blip2/eval/okvqa_ft_qformer_opt.yaml
+source /home/stud/zhangya/jiameng/BLIP2-adalink/env/bin/activate
+python -m torch.distributed.run --nproc_per_node=1 /home/stud/zhangya/jiameng/BLIP2-adalink/evaluate.py --cfg-path /home/stud/zhangya/jiameng/BLIP2-adalink/config_blip2/eval/okvqa_ft_qformer_opt.yaml
--- scripts_slurm/ft_adalink_t5_rank2.sh
+++ scripts_slurm/ft_adalink_t5_rank2.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 #SBATCH --job-name fed
-#SBATCH --output=test_ada_t5_rank2.out
+#SBATCH --output=/nfs/data2/zhangya/slurm/test_ada_t5_rank2.out
 #SBATCH --ntasks=1
 #SBATCH --time=200:00:00
 #SBATCH --gres=gpu:2
 #SBATCH --cpus-per-task=16
-#SBATCH --error=test_ada_t5_rank2.e
+#SBATCH --error=/nfs/data2/zhangya/slurm/test_ada_t5_rank2.e
 #SBATCH --nodelist=worker-6
 
-source ./env/bin/activate
-python -m torch.distributed.run --nproc_per_node=1 train.py --cfg-path config_blip2/train/okvqa_ft_t5_rank2.yaml
+source /home/stud/zhangya/jiameng/BLIP2-adalink/env/bin/activate
+python -m torch.distributed.run --nproc_per_node=1 /home/stud/zhangya/jiameng/BLIP2-adalink/train.py --cfg-path /home/stud/zhangya/jiameng/BLIP2-adalink/config_blip2/train/okvqa_ft_t5_rank2.yaml
