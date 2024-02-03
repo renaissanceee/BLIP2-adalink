21dc2b48cfc4615f38a8041278870e69a6e4873e
renaissanceee <st179481@stud.uni-stuttgart.de>
2024-02-03 18:52:40 +0000

    config

--- scripts_slurm/ft_adalink_t5_v2_rank8.sh
+++ scripts_slurm/ft_adalink_t5_v2_rank8.sh
@@ -10,4 +10,4 @@
 #SBATCH --nodelist=worker-6
 
 source /home/stud/zhangya/jiameng/BLIP2-adalink/env/bin/activate
-python -m torch.distributed.run --nproc_per_node=1 /home/stud/zhangya/jiameng/BLIP2-adalink/train.py --master_port 12332 --cfg-path /home/stud/zhangya/jiameng/BLIP2-adalink/config_blip2/train/vqav2_ft_t5_rank8.yaml
+python -m torch.distributed.run --nproc_per_node=1 /home/stud/zhangya/jiameng/BLIP2-adalink/train.py --cfg-path /home/stud/zhangya/jiameng/BLIP2-adalink/config_blip2/train/vqav2_ft_t5_rank8.yaml
